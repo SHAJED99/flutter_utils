c67d792ff2496291b259f7f4da55f39dd34caf0a
Shajedur Rahman Panna <39647986+SHAJED99@users.noreply.github.com>
2023-10-22 11:21:56 +0600

    Initial commit

---
+++ widgets/time_range_selector_widget/android/app/src/main/kotlin/com/example/time_range_selector_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.time_range_selector_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ widgets/time_range_selector_widget/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:time_range_selector_widget/time_range_selector_widget.dart';
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            alignment: Alignment.center,
+            height: 300,
+            width: 300,
+            child: TimeRangeSelectorWidget(
+              initialTime: 2,
+              maxTime: 24,
+              stockColor: Colors.amber,
+              shadowColorLight: Colors.white.withOpacity(0.5),
+              shadowColorDark: Theme.of(context).shadowColor.withOpacity(0.5),
+              colorGradient: const [
+                Color(0xFFF5F5F9),
+                Color(0xFFE4E8EE),
+              ],
+              backgroundColor: const [
+                Color(0xFFF5F5F9),
+                Color(0xFFE4E8EE),
+              ],
+              onChangeValue: (currentTime) {
+                print(currentTime);
+              },
+              childBuilder: (currentTime) {
+                return Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(currentTime.toString(), style: const TextStyle(fontSize: 70, fontWeight: FontWeight.bold, height: 1)),
+                      const Text("Hour", style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal)),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
